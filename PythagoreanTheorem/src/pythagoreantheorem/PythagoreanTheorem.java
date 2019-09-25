e3ad5fa29f91ade7dde62469364035688e704f3d
unknown <Robert>
2019-09-25 22:06:46 +0200

    first commit

---
+++ PythagoreanTheorem/src/pythagoreantheorem/PythagoreanTheorem.java
@@ -0,0 +1,32 @@
+/*
+this program calculates the hypotenuse
+*/
+package pythagoreantheorem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+/**
+ * @author Robert Masiuk robert@masiuk.pl
+ */
+public class PythagoreanTheorem {
+    public static void main(String[] args) throws IOException {
+        
+        InputStreamReader data = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(data);
+        
+        double a, b, c;
+
+        System.out.println("give first cathetus");
+        a = Double.parseDouble(br.readLine());
+        System.out.println("give second cathetus");
+        b = Double.parseDouble(br.readLine());
+        
+        c = (pow(a, 2)) + (pow(b, 2));
+        c = sqrt(c);
+        System.out.println("Hypotenuse equals: " + c);
+        }
+
+}
